1518ca2f2a167dd4ceaebe2e1075892b99748a8e
zep1994 <snaxbags@protonmail.com>
2019-01-23 15:57:43 -0600

    Day 1 tutorial

--- Car/src/car/Car.java
+++ Car/src/car/Car.java
@@ -7,6 +7,7 @@ public class Car {
     double weight = 4079;
     boolean isTheCarOn = false;
     char condition = 'A';
+    String nameOfCar = "Lucy";
     
     public void printVariables() {
         System.out.println(maxSpeed);
@@ -14,12 +15,24 @@ public class Car {
         System.out.println(weight);
         System.out.println(isTheCarOn);
         System.out.println(condition);
+        System.out.println(nameOfCar);
     }
 
-    
+    public void upgradeMinSpeed() {
+        minSpeed = maxSpeed;
+        maxSpeed = maxSpeed + 1;
+    }
+//    public void wreckCar() {
+//        condition = 'C';
+//    }
+//    
     public static void main(String[] args) {
-        Car carry = new Car();
-        carry.printVariables();
+        Car familyCar = new Car();
+        System.out.println("Family Car");
+        familyCar.printVariables();
+        familyCar.upgradeMinSpeed();
+        familyCar.printVariables();
+
     }
     
 }
