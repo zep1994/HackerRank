eac07f6a82ba01003b77d61aaa4d5637314ce337
zep1994 <snaxbags@protonmail.com>
2019-12-04 12:02:51 -0500

    complete code

--- Counting_Valleys/script.js
+++ Counting_Valleys/script.js
@@ -1,27 +1,13 @@
 
 function countingValleys(n, s) {
-    let steps = s.split('')
-    let path = []
-    let sea_level = 0
-    let valleys = 0
+    let sea_level = 0, valleys = 0
 
-    for (let i of steps) {
-        if (i === "D") {
-            sea_level = sea_level - 1
-            path.push(sea_level)
-        } else if (i === "U") {
-            sea_level = sea_level + 1
-            path.push(sea_level)
-        }
+    for (let i = 0; i < s.length;  i++) {
+        sea_level = (s[i] === "U") ? sea_level + 1 : sea_level - 1
+        valleys = ((sea_level === 0) && (s[i] === "U")) ? valleys += 1 : valleys
     }
 
-    //console.log(path)
-    //console.log(sea_level)
-    //console.log(n, steps)
-
-    for (let j of path) {
-        console.log(j)
-    }
+    console.log(valleys)
 }
 
 countingValleys(8, 'UDDDUDUU')
