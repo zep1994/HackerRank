35c5739be088df39b8b060fab045bbe5eacae319
zep1994 <snaxbags@protonmail.com>
2019-12-11 23:14:14 -0500

    first part complete

--- New Years Day Line/script.js
+++ New Years Day Line/script.js
@@ -1,5 +1,16 @@
 // Complete the minimumBribes function below.
 function minimumBribes(q) {
-    
+    //define counter for number of moves
+    let moves = 0
+
+    //Determine if player has moved more than once 
+    for (let i = 0; i < q.length; i++) {
+        if (q[i] - (i + 1) > 2) {
+            console.log("Too Chaotic")
+        }
+    }
+    //Determine if player has been bribed 
 
 }
+
+minimumBribes([5, 1, 2, 3, 7, 8, 6, 4])
